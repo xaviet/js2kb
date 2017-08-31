23b4c3eee9a91743d8a6cd43cc43859a30f94441
jin xu <pioevh@163.com>
2017-08-31 20:46:44 +0800

    done

---
+++ js2kb/resource.h
@@ -0,0 +1,42 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 js2kb.rc 使用
+//
+#define IDD_JS2KB_DIALOG                102
+#define IDR_MAINFRAME                   128
+#define IDC_CHECK1                      1000
+#define IDC_CHECK2                      1001
+#define IDC_CHECK3                      1002
+#define IDC_CHECK4                      1003
+#define IDC_CHECK5                      1004
+#define IDC_CHECK6                      1005
+#define IDC_CHECK7                      1006
+#define IDC_CHECK8                      1007
+#define IDC_CHECK9                      1008
+#define IDC_CHECK10                     1009
+#define IDC_CHECK11                     1010
+#define IDC_CHECK12                     1011
+#define IDC_EDIT2                       1013
+#define IDC_EDIT3                       1014
+#define IDC_EDIT4                       1015
+#define IDC_EDIT5                       1016
+#define IDC_EDIT6                       1017
+#define IDC_EDIT7                       1018
+#define IDC_EDIT8                       1019
+#define IDC_EDIT9                       1020
+#define IDC_EDIT10                      1021
+#define IDC_EDIT11                      1022
+#define IDC_EDIT12                      1023
+#define IDC_EDIT13                      1024
+#define IDC_EDIT1                       1025
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1026
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
